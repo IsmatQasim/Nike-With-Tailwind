d86617dc5d8b962cf5b2dcb9b41e463153310126
IsmatQasim <ismat6812@gmail.com>
2024-01-28 23:27:53 +0500

    commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,40 @@
+import { headerLogo } from '/assets/images';
+import { navLinks } from '../constants';
+const Nav = () => {
+  return (
+    <header className='padding-x
+    py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between 
+        items-center max-container'>
+            <a href="/">
+                <img 
+                src={headerLogo}
+                alt='Logo'
+                width={130}
+                height={29}
+                />
+            </a>
+            <ul className='flex-1 flex
+            justify-center items-center
+            gap-16 max-lg:hidden'>
+                {navLinks.map((item) =>(
+                    <li key={item.label}>
+                        <a
+                        href={item.href}
+                        className='font-monts
+                        errat leading-normal
+                        text-lg
+                        text-slate-grey
+                        '>
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
